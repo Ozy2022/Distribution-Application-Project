695ec28f3ffaf30662551938758478cec24622ad
CalvinAH <calvinbusiness24@gmail.com>
2022-05-20 13:18:31 +0800

    Initiate 1

---
+++ RMIClient/RMIClient.java
@@ -0,0 +1,18 @@
+
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import shared.ClientInterface;
+import shared.MessageToClient;
+import shared.UcBuscaInterface;
+
+public class RMIClient extends UnicastRemoteObject implements ClientInterface {
+	
+
+}
