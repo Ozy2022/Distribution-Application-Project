196663021e1bf81043234bde0a0b5577fb335a47
CalvinAH <calvinbusiness24@gmail.com>
2022-05-20 14:00:04 +0800

    Compute Item class push

---
+++ ComItems.java
@@ -0,0 +1,58 @@
+public class ComItems implements Serializable{
+    private static final long serialVersionUID = 12351313553L;
+    private String itemName;
+    private String code; //item id
+    private double price;
+    private int stock;
+
+    
+    public ComItems(String code){
+        this.code = code;
+    }
+    public ComItems(String itemName, String code, double price, int stock) {
+        this.itemName = itemName;
+        this.code = code;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public ComItems() {
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getStock() {
+        return stock;
+    }
+
+    public void setStock(double stock) {
+        this.stock = stock;
+    }
+    @Override
+    public String toString() {
+        return "> " + this.itemName+ " ($" + this.price +"    Stock:"+this.stock+ ")";
+    }
+}
+    
